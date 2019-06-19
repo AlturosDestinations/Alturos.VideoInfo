f431eee8a878509ad04d8607881a22ab12a786e1
Tino Hager <tino.hager@alturos.com>
2019-06-19 17:38:55 +0200

    Optimize documentation, add some summary

--- README.md
+++ README.md
@@ -15,5 +15,19 @@ var videoFilePath = "myVideo.mp4";
 
 var videoAnalyer = new VideoAnalyzer();
 var videoInfo = videoAnalyer.GetVideoInfo(videoFilePath);
+
+//videoInfo.Format.Filename = "TestVideos\\video1.mp4"
+//videoInfo.Format.NbStreams = 1
+//videoInfo.Format.NbPrograms = 0
+//videoInfo.Format.FormatName = "mov,mp4,m4a,3gp,3g2,mj2"
+//videoInfo.Format.FormatLongName = "QuickTime / MOV"
+//videoInfo.Format.StartTime = 0
+//videoInfo.Format.Duration = 120 //seconds
+//videoInfo.Format.Size = 2088470 //bytes
+//videoInfo.Format.BitRate = 139231
+//videoInfo.Format.ProbeScore = 100
+//videoInfo.Format.Tags["encoder"] = Lavf57.76.100
+//videoInfo.Streams[0].CodecType = "video" //Video, Audio
+//videoInfo.Streams[0]...
 ```
 
--- src/Alturos.VideoInfo/Model/Format.cs
+++ src/Alturos.VideoInfo/Model/Format.cs
@@ -10,9 +10,20 @@ namespace Alturos.VideoInfo.Model
         public string FormatName { get; set; }
         public string FormatLongName { get; set; }
         public double StartTime { get; set; }
+        /// <summary>
+        /// The duration of the file
+        /// </summary>
         public double Duration { get; set; }
+        /// <summary>
+        /// The size of the file in bytes
+        /// </summary>
         public long Size { get; set; }
         public int BitRate { get; set; }
+        /// <summary>
+        /// ProbeScore 0-100
+        /// 100 is the maximum score, meaning that FFmpeg is sure that the format is the real one.
+        /// With a score below 25, it is recommanded to increase probe duration.
+        /// </summary>
         public int ProbeScore { get; set; }
         public Dictionary<string, string> Tags { get; set; }
     }
