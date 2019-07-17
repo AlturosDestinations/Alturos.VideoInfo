1c1c92e464bea6f213880c3d8404f77f75f912e2
Lukas Kneringer <lukas.kneringer@alturos.com>
2019-07-17 10:25:27 +0200

    Cleanup

--- src/Alturos.VideoInfo/FileDownloader.cs
+++ src/Alturos.VideoInfo/FileDownloader.cs
@@ -60,6 +60,11 @@ namespace Alturos.VideoInfo
             var folderName = Path.GetFileNameWithoutExtension(fileName);
             var folderPath = Path.Combine(destinationPath, folderName);
 
+            if (!Directory.Exists(destinationPath))
+            {
+                Directory.CreateDirectory(destinationPath);
+            }
+
             using (var httpClient = new HttpClient())
             {
                 httpClient.Timeout = TimeSpan.FromMinutes(10);
@@ -76,35 +81,30 @@ namespace Alturos.VideoInfo
 
                     var fileContentStream = await httpResponseMessage.Content.ReadAsStreamAsync().ConfigureAwait(false);
 
-                    if (!Directory.Exists(destinationPath))
-                    {
-                        Directory.CreateDirectory(destinationPath);
-                    }
-
                     using (var sourceStream = new FileStream(filePath, FileMode.Append, FileAccess.Write, FileShare.None, bufferSize: 4096, useAsync: true))
                     {
                         fileContentStream.Seek(0, SeekOrigin.Begin);
                         await fileContentStream.CopyToAsync(sourceStream);
                     }
-
-                    if (Directory.Exists(folderPath))
-                    {
-                        Directory.Delete(folderPath, true);
-                    }
-
-                    ZipFile.ExtractToDirectory(filePath, folderPath);
-                    File.Delete(filePath);
-
-                    var basePath = Path.Combine(destinationPath, folderName, folderName, "bin");
-                    return new FfmpegDownloadResult
-                    {
-                        Successful = true,
-                        FfmpegPath = Path.Combine(basePath, "ffmpeg.exe"),
-                        FfprobePath = Path.Combine(basePath, "ffprobe.exe"),
-                        FfplayPath = Path.Combine(basePath, "ffplay.exe")
-                    };
                 }
             }
+
+            if (Directory.Exists(folderPath))
+            {
+                Directory.Delete(folderPath, true);
+            }
+
+            ZipFile.ExtractToDirectory(filePath, folderPath);
+            File.Delete(filePath);
+
+            var basePath = Path.Combine(destinationPath, folderName, folderName, "bin");
+            return new FfmpegDownloadResult
+            {
+                Successful = true,
+                FfmpegPath = Path.Combine(basePath, "ffmpeg.exe"),
+                FfprobePath = Path.Combine(basePath, "ffprobe.exe"),
+                FfplayPath = Path.Combine(basePath, "ffplay.exe")
+            };
         }
     }
 }
