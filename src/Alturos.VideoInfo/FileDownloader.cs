759f5b9409587a44bdb299c53013614757485715
Lukas Kneringer <lukas.kneringer@alturos.com>
2019-07-17 10:15:04 +0200

    Download Result

--- src/Alturos.VideoInfo/FileDownloader.cs
+++ src/Alturos.VideoInfo/FileDownloader.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Alturos.VideoInfo.Model;
+using System;
 using System.IO;
+using System.IO.Compression;
 using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
@@ -49,11 +51,14 @@ namespace Alturos.VideoInfo
             throw new NotSupportedException("Binary download is not supported");
         }
 
-        public async Task<bool> DownloadAsync(string url, string destinationPath)
+        public async Task<FfmpegDownloadResult> DownloadAsync(string url, string destinationPath)
         {
             var uri = new Uri(url);
             var fileName = Path.GetFileName(uri.LocalPath);
             var filePath = Path.Combine(destinationPath, fileName);
+
+            var folderName = Path.GetFileNameWithoutExtension(fileName);
+            var folderPath = Path.Combine(destinationPath, folderName);
 
             using (var httpClient = new HttpClient())
             {
@@ -63,17 +68,41 @@ namespace Alturos.VideoInfo
                 {
                     if (!httpResponseMessage.IsSuccessStatusCode)
                     {
-                        return false; ;
+                        return new FfmpegDownloadResult
+                        {
+                            Successful = false
+                        };
                     }
 
                     var fileContentStream = await httpResponseMessage.Content.ReadAsStreamAsync().ConfigureAwait(false);
+
+                    if (!Directory.Exists(destinationPath))
+                    {
+                        Directory.CreateDirectory(destinationPath);
+                    }
+
                     using (var sourceStream = new FileStream(filePath, FileMode.Append, FileAccess.Write, FileShare.None, bufferSize: 4096, useAsync: true))
                     {
                         fileContentStream.Seek(0, SeekOrigin.Begin);
                         await fileContentStream.CopyToAsync(sourceStream);
                     }
 
-                    return true;
+                    if (Directory.Exists(folderPath))
+                    {
+                        Directory.Delete(folderPath, true);
+                    }
+
+                    ZipFile.ExtractToDirectory(filePath, folderPath);
+                    File.Delete(filePath);
+
+                    var basePath = Path.Combine(destinationPath, folderName, folderName, "bin");
+                    return new FfmpegDownloadResult
+                    {
+                        Successful = true,
+                        FfmpegPath = Path.Combine(basePath, "ffmpeg.exe"),
+                        FfprobePath = Path.Combine(basePath, "ffprobe.exe"),
+                        FfplayPath = Path.Combine(basePath, "ffplay.exe")
+                    };
                 }
             }
         }
