6b3c71083e372e50b856f0cccb3ce96e8ff78c0d
LKneringer <48755364+LKneringer@users.noreply.github.com>
2019-07-17 09:02:57 +0200

    Create FfmpegDownloadResult.cs

---
+++ src/Alturos.VideoInfo/Model/FfmpegDownloadResult.cs
@@ -0,0 +1,10 @@
+namespace Alturos.VideoInfo.Model
+{
+    public class FfmpegDownloadResult
+    {
+        public bool Successful { get; set; }
+        public string FfmpegPath { get; set; }
+        public string FfprobePath { get; set; }
+        public string FfplayPath { get; set; }
+    }
+}
